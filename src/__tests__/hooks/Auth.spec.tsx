dba42b501aa3df09b308a92cb34563bcab9dfa0e
ygorthiago <ygorthiagoff@outlook.com>
2021-01-03 18:06:59 -0300

    test: should be able to sign in

---
+++ src/__tests__/hooks/Auth.spec.tsx
@@ -0,0 +1,39 @@
+import { renderHook } from "@testing-library/react-hooks";
+import { AuthProvider, useAuth } from "../../hooks/Auth";
+import MockAdapter from "axios-mock-adapter";
+import api from "../../services/apiClient";
+
+const apiMock = new MockAdapter(api);
+
+describe('Auth hook', () => {
+  it('should be able to sign in', async () => {
+    const apiResponse = {
+      user: {
+        id: 'user123',
+        name: 'John Doe',
+        email: 'johndoe@example.com'
+      },
+      token: 'token-123',
+    }
+
+    apiMock.onPost('sessions').reply(200, apiResponse);
+
+    const setItemSpy = jest.spyOn(Storage.prototype, 'setItem');
+
+    const { result, waitForNextUpdate } = renderHook(() => useAuth(), {
+      wrapper: AuthProvider,
+    });
+
+    result.current.signIn({
+      email: 'johndoe@example.com',
+      password: '123123',
+    });
+
+    await waitForNextUpdate();
+
+    expect(setItemSpy).toHaveBeenCalledWith('@GoBarber:token', apiResponse.token);
+    expect(setItemSpy).toHaveBeenCalledWith('@GoBarber:user', JSON.stringify(apiResponse.user));
+
+    expect(result.current.user.email).toEqual('johndoe@example.com');
+  });
+});
